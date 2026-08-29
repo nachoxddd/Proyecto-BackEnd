0c1fecd0278f288e84c09b43c0af498b753aa168
nachoxddd <aim.igna14@gmail.com>
2026-08-28 21:08:54 -0400

    prueba1

--- IEI_N4_C1/mantenciones_django/settings.py
+++ IEI_N4_C1/mantenciones_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ft^1-i%rt8%h7j2vxh057b4_6e_$wm=iqbgbaon)zpt*dke4x#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mantenciones_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
