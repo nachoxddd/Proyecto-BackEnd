a780f54cd8106ac7b832bf296621d1d23543684c
nachoxddd <aim.igna14@gmail.com>
2026-09-04 12:03:13 -0400

    bcspn

--- mantenciones_django/urls.py
+++ mantenciones_django/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from appMantenciones.views import home  # <-- Importar tu vista
+from appMantenciones import views  # <-- Importar tu vista
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),       # <-- Conectar la ruta vacía al home
+    path('', views.home, name='home'),       # <-- Conectar la ruta vacía al home
 ]
